98739d261527f130173c8de03c80a9d4d126ce26
Fabian Santavicca <fafunas@gmail.com>
2024-11-04 22:41:44 -0300

    Coneccicon con back

--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="web",
+    api_url="https://cervesia.up.railway.app:8000"
 )
--- src/web/web.py
+++
@@ -1,9 +0,0 @@
-
-import reflex as rx
-from web.pages.dashboard import index
-from web.pages.products import products
-
-from rxconfig import config
-
-
-app = rx.App()
