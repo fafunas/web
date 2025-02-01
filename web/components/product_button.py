c372153d374a47a75941fd80d3bb787597395c24
Fabian Santavicca <fafunas@gmail.com>
2025-02-01 13:09:44 -0300

    Changes product layout

--- web/components/product_button.py
+++ web/components/product_button.py
@@ -3,36 +3,14 @@ from ..states.productState import ProductState
 
 
 def product_button() -> rx.Component:
-    return rx.dialog.root(
-        rx.dialog.trigger(
-            rx.button(
-                rx.icon("plus", size=26),
-                rx.text("Agregar Producto", size="4"),
-                size="3",
-            ),
-        ),
-        rx.dialog.content(
-            rx.dialog.title("Agregar Producto"),
-            rx.dialog.description("Agregar un nuevo producto a la lista"),
-            # rx.hstack(
-            #     rx.badge(
-            #         rx.icon(tag="package", size=20),
-            #         color_scheme="blue",
-            #         radius="full",
-            #         padding="0.65rem",
-            #     ),
-            #     rx.vstack(rx.dialog.title(" Agregar Producto")),
-            # ),
-            rx.form(
+    return rx.form(
                 rx.flex(
                     rx.vstack(
                         rx.input(placeholder="Producto", name="name", required=True),
                         rx.input(placeholder="Precio", name="price", required=True),
                         rx.flex(
-                            rx.dialog.close(
-                                rx.button("Cancel", variant="soft", color_scheme="gray")
-                            ),
-                            rx.dialog.close(rx.button("Agregar", type="submit")),
+                           
+                            rx.button("Agregar", type="submit"),
                             spacing="3",
                             justify="end",
                         ),
@@ -42,5 +20,3 @@ def product_button() -> rx.Component:
                 on_submit=ProductState.createProduct,
                 reset_on_submit=False,
             ),
-        ),
-    )
